6c3cd26bdb0c0eb46083792d476a8fe446074623
Ajay <106470358+PbAjay@users.noreply.github.com>
2022-12-09 09:30:52 +0530

    Credits Done

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1059,7 +1059,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data == "owner_info":
             btn = [[
                     InlineKeyboardButton("⟸ Bᴀᴄᴋ", callback_data="start"),
-                    InlineKeyboardButton("Cᴏɴᴛᴀᴄᴛ", url="t.me/creatorbeatz")
+                    InlineKeyboardButton("Cᴏɴᴛᴀᴄᴛ", url="t.me/MatRixBotz_TG")
                   ]]
             await client.edit_message_media(
                 query.message.chat.id, 
