08a6156a6ec3146eb580569f4933944318edcf2f
Hansaka Anuhas <120485409+HansakaAnuhas@users.noreply.github.com>
2022-12-18 09:17:39 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/d65ff6f3/Matrix-01
+  git clone https://github.com/d65ff6f3/Matrix-01.git /Matrix-01
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Matrix-01
 fi
 cd /Matrix-01
 pip3 install -U -r requirements.txt
-echo "Starting DQ-The-File-Donor...."
+echo "Starting Bot..."
 python3 bot.py
